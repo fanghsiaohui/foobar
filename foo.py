26942889d84ed13946dabc47d3ea02583eab4069
fang <fang@win10>
2018-09-06 17:20:49 +0800

    4

--- foo.py
+++ foo.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-def fibo(n):
-    print("hello, python")
-    f = []
-    a, b = 0, 1
-    while b <= n:
-        print(b, end=' ')
-        f.append(b)
-        a, b = b, a+b
-    print()
-    return f
-
-y1 = fibo(60)
-print("add a number")
-y1.append(100)
-print(y1)
+for n in range(2,10):
+    k = 0
+    for x in range(2,n):
+        if n % x == 0:
+            print(n, 'euqals', x, '*', n//x)
+            k = 1
+            break
+    if k == 0:
+        print(n, 'is a prime number')
