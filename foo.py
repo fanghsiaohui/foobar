aea108b5de28012c485acd0efddd911f992220d4
fang <fang@arch>
2018-09-09 02:03:35 +0800

    py

--- foo.py
+++ foo.py
@@ -1,4 +1,39 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- do you have any", kind, "?")
+    print("-- i'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("-" * 40)
+    keys = sorted(keywords.keys())
+    for kw in keys:
+        print(kw, ":", keywords[kw])
+'''
+
+'''
+def parrot(voltage, state='a stiff', action='voom', type='Norwegian Blue'):
+    print("-- this parrot would't", action, end=' ')
+    print("if you put", voltage, "volts through it.")
+    print("-- lovely plumage, the", type)
+    print("-- it's:", state, "!")
+'''
+
+'''
+def ask_ok(prompt, retries=4, complaint='Yes or no, plesae!'):
+    while True:
+        ok = input(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return True
+        if ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise OSError('uncooprative user')
+        print(complaint)
+'''
+
 '''
 for n in range(2,10):
     k = 0
@@ -16,9 +51,10 @@ def fib(n):
         a, b = b, a+b
     print()
 '''
+'''
 def f(a, L=[]):
     if L == []:
         L = []
     L.append(a)
     return L
-
+'''
