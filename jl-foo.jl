648837c100225573794bd1bb962c40ccba65456a
fang <fang@arch>
2018-09-13 18:56:31 +0800

    reezet

---
+++ jl-foo.jl
@@ -0,0 +1,7 @@
+# julia test file
+println("hello,$PROGRAM_FILE")
+println(ARGS)
+for i in ARGS
+	println(i)
+end
+
--- py-foo.py
+++ py-foo.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import sys
+for i in sys.argv:
+    print(i)
+
+print(sys.argv[0])
