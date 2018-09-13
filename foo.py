40e03087e891a84f7cafdeb8edc21c8287a641f6
fang <fang@arch>
2018-09-13 10:58:05 +0800

    reset foo file

--- foo.py
+++ foo.py
@@ -1,9 +1,2 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-class Student(object):
-    def __init__(self, name):
-        self.name = name
-    def __str__(self):
-        return 'HHHHHHH oooooo (name: %s)' % self.name
-
