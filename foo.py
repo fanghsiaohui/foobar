e77a0cfd123c9b83d94c92a0c6cb22d85c3dde2f
fang <fang@arch>
2018-09-22 15:28:17 +0800

    py

---
+++ foo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#import pdb
+#pdb.set_trace()
+# d = {'aa':'first',
+#         'bb':'second',
+#         'cc':'third'
+#         }
+# l = ['xx', 'yy', 'zz']
+# t = ('mm', 'nn', 'tt')
+# 
+# a, b, c = l
+# #print('{0}\n{1}\n{2}\n'.format(d,l,t))
+# print(a, b)
+x = range(1,10)
+y = [i**2 for i in x]
+print(x)
+print(y)
