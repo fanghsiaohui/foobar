08fc2dd1cba0fc59e060403f3696b6487db0daeb
fang <fang@arch>
2018-09-26 14:28:24 +0800

    py-bit

--- bar.py
+++
@@ -1,8 +0,0 @@
-import random
-print(random.choice(['C++', 'Java', 'Python']))
-print(random.randint(1, 100))
-print(random.randrange(0, 10, 2))
-print(random.random())
-print(random.uniform(5, 10))
-print(random.sample(range(100), 10))
-print(random.shuffle(list))
