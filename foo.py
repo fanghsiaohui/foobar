58d6e94376c82521ef3e3a0d16b3343ff6308f5c
fang <fang@arch>
2018-09-29 16:20:37 +0800

    m

--- foo.py
+++ foo.py
@@ -1,32 +1,15 @@
 #!/usr/bin/env python3
 # coding: utf-8
  
-# import pdb
+import pdb
 # pdb.set_trace()
 
-import time
-
-def decorator(func):
-    def wrapper(*args, **kw):
-        print(time.time())
-        func(*args, **kw)
-    return wrapper
-
-@decorator
-def f1(func_name):
-    print("this is a function named " + func_name)
-
-@decorator
-def f2(func_name1, func_name2):
-    print("this is a function named " + func_name1)
-    print("this is a function named " + func_name2)
-
-@decorator
-def f3(func_name1, func_name2, **kw):
-    print("this is a function named " + func_name1)
-    print("this is a function named " + func_name2)
-    print(kw)
-
-f1("test func")
-f2("test func1", "test func2")
-f3("test func1", "test_func2", a=1, b=2, c="1.2.3"))
+s = 0
+pdb.set_trace()
+for i in range(10):
+    s += i
+print(i, s)
+pdb.set_trace()
+print("hello")
+pdb.set_trace()
+print("world")
