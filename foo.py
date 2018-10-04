7b7185d011ba7861286eff96b8ac662fcc0fad68
fang <fang@win10>
2018-10-04 11:45:14 +0800

    decro

--- foo.py
+++ foo.py
@@ -5,6 +5,18 @@
 # import pdb
 # pdb.set_trace()
 
-print("first line")
-print("second line")
-print("third line")
+def deco(func):
+    print("before")
+    def func():
+        print("after")
+    return func
+
+def add(args):
+    s = sum(args)
+    print("sum = {}".format(s))
+    return s
+
+x = [1,2,3]
+
+@deco
+add(x)
