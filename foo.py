9f1421e184a0ea864779081ae0a741b416d49a71
fang <fang@arch>
2018-10-06 11:27:12 +0800

    filter

--- foo.py
+++ foo.py
@@ -3,16 +3,18 @@
  
 # import pdb
 # pdb.set_trace()
-# import sys, re, time
+# import sys, re, time, os, shutil, command
+# from functools import wraps
+import scipy, sympy
 import numpy as np
 import matplotlib.pyplot as plt
 
-# code below
 x = np.linspace(1,10,1000)
 y = np.sin(x)
 plt.figure()
-plt.plot(x, y, label="中文chinese", color="red")
+plt.plot(x, y, label="chinese", color="red")
 plt.ylim(-2,2)
-plt.ylabel("哈哈haha")
+plt.ylabel("haha")
 plt.legend()
 plt.show()
+
