dc5ff55ac2817292cf88e9f268882df33c1380fa
fang <fang@arch>
2018-10-13 23:01:02 +0800

    rm

--- foo.c
+++ foo.c
@@ -1,26 +1,4 @@
 #include <stdio.h>
 
 int main(){
-    int n, m;
-    scanf("%d %d", &n, &m);
-
-    int prime =1;
-    int sum = 0;
-    int x = 2;
-    int i = 1;
-    while (i <= m){
-        sum += (i >= n) * prime * x;
-        prime = 1;
-        x ++;
-        for (int j = 2; j < x; j++){
-            if (x % j ==0){
-                prime = 0;
-                break;
-            }
-        }
-        i += prime;
-    }
-    printf("%d\n", sum);
-    return 0;
 }
-
--- foo.py
+++ foo.py
@@ -6,12 +6,3 @@
 # import sys, re, time, os, shutil, command
 # from functools import wraps
 
-def countchar(string):
-    alphabet = {chr(ord('a') + i) : 0 for i in range(26)}
-    for i in alphabet:
-        alphabet[i] = string.lower().count(i)
-    return [value for key, value in sorted(alphabet.items())]
-
-if __name__ == "__main__":
-    string = input()
-    print(countchar(string))
