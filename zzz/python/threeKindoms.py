d020d04faa9652a1304e09a4ef431d12e8c93e64
fang <fang@arch>
2018-10-24 18:34:46 +0800

    mldify

--- zzz/python/threeKindoms.py
+++ zzz/python/threeKindoms.py
@@ -9,10 +9,32 @@
 
 import jieba
 txt = open("threekingdoms.txt", "r", encoding = "utf-8").read()
-words
+excludes = {"", "", "", "", ""}
+words = jieba.lcut(txt)
 counts = {}
 for word in words:
     if len(word) == 1:
         continue
+    elif word == "" or word == "":
+        rword = ""
+    elif word == "" or word == "":
+        rword = ""
+    elif word == "" or word == "":
+        rword = ""
+    elif word == "" or word == "":
+        rword = ""
+    elif word == "" or word == "":
+        rword = ""
+    elif word == "" or word == "":
+        rword = ""
     else:
-        counts[word] = counts.get(word, 0) + 1
+        rword = word
+    counts[rword] = counts.get(rword, 0) + 1
+for word in excludes:
+    del counts[word]
+items = list(counts.items())
+items.sort(key = lambda x:x[1], reverse = True)
+for i in range(10):
+    word, count = items[i]
+    print("{0:<10}: {1:>5}".format(word, count))
+
