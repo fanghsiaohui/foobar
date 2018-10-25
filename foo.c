c394b8bb1254b53bb248a83b170b59d59958024b
fang <fang@arch>
2018-10-25 17:48:41 +0800

    l

--- foo.c
+++ foo.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 int main(){
-    int count[5] = {};
-    for (int i = 0; i < 5; i ++){
-        printf("%d\n", count[i]);
-    }
     printf("hello, c\n");
     return 0;
 }
