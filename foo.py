a4dd6aed94b650424049999afaea9b2bd66be083
fang <fang@arch>
2018-11-09 03:35:51 +0800

    delete space line

--- foo.py
+++ foo.py
@@ -5,12 +5,3 @@
 # import sys, re, time, os, shutil, command
 # from functools import wraps
 # print(*(enumerate(range(10))))
-
-__metaclass__ = type
-
-class C:
-    print("ccccc")
-
-print(str.__base__)
-print(object.__base__)
-
--- foo.sh
+++ foo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+
 for i in $(seq 10)
 do
     echo $i
     sleep 0.2
 done
-
