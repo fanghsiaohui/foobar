0dd7e443b731c7a71be18f4d6c0cd16859a45226
fang <fang@arch>
2018-11-11 00:16:13 +0800

    d

--- foo.py
+++ foo.py
@@ -6,25 +6,10 @@
 # from functools import wraps
 # print(*(enumerate(range(10))))
 
-import turtle as t
-
-t.title("auto trace")
-t.setup(800, 600, 0, 0)
-t.pencolor("red")
-t.pensize(5)
-
-datals = []
-f = open("data", "r")
-for line in f:
-    line = line.replace("\n", "")
-    datals.append(list(map(eval, line.split(","))))
-f.close()
-
-for i in range(len(datals)):
-    t.pencolor(datals[i][3], datals[i][4], datals[i][5])
-    t.fd(datals[i][0])
-    if datals[i][1]:
-        t.right(datals[i][2])
-    else:
-        t.left(datals[i][2])
-t.done()
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.linspace(1,10,100)
+y = np.sin(x)
+plt.figure()
+plt.plot(x, y)
+plt.show()
