d22e4d094f8447285a280d8629e59c63ca4a7984
fang <fang@arch>
2018-11-14 05:10:15 +0800

    jl

--- foo.jl
+++ foo.jl
@@ -1,3 +1,13 @@
 #!/usr/bin/julia
-#import Dates
-println("hello, julia")
+using UnicodePlots
+x = rand(10)
+println(x)
+println("line")
+p1 = lineplot(x)
+display(p1)
+println("scatter")
+p2 = scatterplot(x)
+display(p2)
+println("histogram")
+p3 = histogram(x)
+display(p3)
--- foo.py
+++ foo.py
@@ -9,7 +9,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 x = np.linspace(1,10,100)
-y = np.sin(x)
+y1 = np.sin(x)
+y2 = np.log(x)
+y3 = x**2
+y4 = x**3
 plt.figure()
-plt.plot(x, y)
+plt.subplot(221)
+plt.plot(x, y1)
+plt.subplot(222)
+plt.plot(x, y2)
+plt.subplot(223)
+plt.plot(x, y3)
+plt.subplot(224)
+plt.plot(x, y4)
 plt.show()
