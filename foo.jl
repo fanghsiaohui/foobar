275595a56857fce9fc822f9b60a7f4035f540482
fang <fang@arch>
2018-11-16 01:53:46 +0800

    a

--- foo.jl
+++ foo.jl
@@ -1,13 +1,5 @@
 #!/usr/bin/julia
-using UnicodePlots
-x = rand(10)
-println(x)
-println("line")
-p1 = lineplot(x)
-display(p1)
-println("scatter")
-p2 = scatterplot(x)
-display(p2)
-println("histogram")
-p3 = histogram(x)
-display(p3)
+a=open("d","r")
+l1=read(a,String)
+println(l1)
+println(typeof(l1))
--- foo.py
+++ foo.py
@@ -5,21 +5,3 @@
 # import sys, re, time, os, shutil, command
 # from functools import wraps
 # print(*(enumerate(range(10))))
-
-import numpy as np
-import matplotlib.pyplot as plt
-x = np.linspace(1,10,100)
-y1 = np.sin(x)
-y2 = np.log(x)
-y3 = x**2
-y4 = x**3
-plt.figure()
-plt.subplot(221)
-plt.plot(x, y1)
-plt.subplot(222)
-plt.plot(x, y2)
-plt.subplot(223)
-plt.plot(x, y3)
-plt.subplot(224)
-plt.plot(x, y4)
-plt.show()
