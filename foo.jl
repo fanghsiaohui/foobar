df058237975922fc0f13c49256c4e9ee8adfd4ad
fang <fang@arch>
2018-11-17 07:29:46 +0800

    jl

--- foo.jl
+++ foo.jl
@@ -1,9 +1,18 @@
 #!/usr/bin/julia
-while true
-    line=readline()
-    if line == ""
-        break
-    end
-    println(line)
+function sayhi(name)
+    println("hi $name, it's great to see you!")
 end
-println("ending")
+
+function f(x)
+    x^2
+end
+
+sayhi2(name)=println("hi $name, it's great to see you")
+f2(x)=x^2
+
+println("enter name: ")
+name=readline()
+sayhi(name)
+println(f(42))
+sayhi2(name)
+println(f2(42))
