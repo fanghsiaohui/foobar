1c9489d21b7614d54bbd7f4dd22e19a405a4068f
fang <fang@arch>
2018-11-17 11:02:09 +0800

    sportsScore

--- foo.jl
+++ foo.jl
@@ -1,18 +1,1 @@
 #!/usr/bin/julia
-function sayhi(name)
-    println("hi $name, it's great to see you!")
-end
-
-function f(x)
-    x^2
-end
-
-sayhi2(name)=println("hi $name, it's great to see you")
-f2(x)=x^2
-
-println("enter name: ")
-name=readline()
-sayhi(name)
-println(f(42))
-sayhi2(name)
-println(f2(42))
