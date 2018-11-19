daf4b3fc0f52be04747ece63a69f5f3bbebc419a
fang <fang@arch>
2018-11-19 18:59:23 +0800

    py

--- foo.py
+++ foo.py
@@ -4,3 +4,19 @@
 # import sys, re, time, os, shutil, command
 # from functools import wraps
 # print(*(enumerate(range(10))))
+import turtle as tt
+import time
+tt.setup(16*60,9*60,200,200)
+tt.penup()
+tt.pencolor("red")
+tt.pensize(10)
+tt.fd(-300)
+tt.pendown()
+tt.fd(100)
+tt.left(90)
+tt.fd(50)
+tt.right(90)
+tt.fd(80)
+tt.hideturtle()
+time.sleep(1)
+print(dir("turtle"))
