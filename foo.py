1c9675307136eb8c74ae045b94579fcd60d31c25
fang <fang@arch>
2018-11-27 18:51:53 +0800

    py

--- foo.py
+++ foo.py
@@ -6,7 +6,13 @@
 # print(*(enumerate(range(10))))
 
 import sys
-print("system standard input: ", end="")
-sys.stdout.flush()
-a=sys.stdin.read()
-print("a=", a.strip())
+import numpy as np
+import matplotlib.pyplot as plt
+
+x=np.arange(0,20,0.01)
+y=np.sin(x)
+plt.figure(figsize=(12,9))
+plt.plot(x,y,"r-.")
+plt.savefig("pic",dpi=800)
+plt.show()
+plt.close()
