ccfcf67ecd45ce52184b417a3d945b0d08c30e59
fang <fang@arch>
2018-11-28 21:44:22 +0800

    mode

--- foo.py
+++ foo.py
@@ -5,10 +5,3 @@
 # from functools import wraps
 # print(*(enumerate(range(10))))
 
-import requests, bs4, scrapy
-from bs4 import BeautifulSoup
-
-url = "http://127.0.0.1"
-r = requests.get(url)
-demo = r.text
-soup = BeautifulSoup(demo, "html.parser")
