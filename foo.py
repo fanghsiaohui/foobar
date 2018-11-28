a2e494251653579d2cff37d1a0beb8f0f89a964a
fang <fang@arch>
2018-11-28 16:38:44 +0800

    pc

--- foo.py
+++ foo.py
@@ -5,14 +5,9 @@
 # from functools import wraps
 # print(*(enumerate(range(10))))
 
-import sys
-import numpy as np
-import matplotlib.pyplot as plt
-
-x=np.arange(0,20,0.01)
-y=np.sin(x)
-plt.figure(figsize=(12,9))
-plt.plot(x,y,"r-.")
-plt.savefig("pic",dpi=800)
-plt.show()
-plt.close()
+from bs4 import BeautifulSoup
+import requests
+url = "http://127.0.0.1"
+r = requests.get(url)
+demo = r.text
+soup = BeautifulSoup(demo, "html.parser")
