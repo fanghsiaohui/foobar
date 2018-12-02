1415a173bbbf8666792542f28fed5c99d784cd97
fang <fang@arch>
2018-12-02 18:25:45 +0800

    before pygame

--- foo.py
+++ foo.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-# import pdb
+# import pdb, sys, re, time, os, shutil, command
 # pdb.set_trace()
-# import sys, re, time, os, shutil, command
 # from functools import wraps
 # print(*(enumerate(range(10))))
 
-def hello():
-    print("hello, world")
+import tkinter
+def func():
+    pass
 
 def test():
-    hello()
-    
+    win = tkinter.Tk()
+    win.title("tk title")
+    win.geometry("900x600")
+    btn = tkinter.Button(win, text = "tk btn", command = func())
+    btn.pack(expand = 1, fill = "y")
+    tkinter.mainloop()
+
 if __name__ == "__main__":
     test()
