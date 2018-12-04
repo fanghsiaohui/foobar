a0ccf6c459ab5b5fc9497a3dfd375984b4f028a9
fang <fang@arch>
2018-12-05 05:52:00 +0800

    foo py init

--- foo.py
+++ foo.py
@@ -7,17 +7,7 @@
 import random
 
 def main():
-    cnt = 0
-    for i in range(1000):
-        j = random.randrange(2)
-        if j: cnt += 1
-        print("{:3d}: nu = {}, cnt = {}, {:.6%}".\
-                format(i+1, j, cnt, cnt/(i+1)))
-        a = input()
-        try:
-            if a[0] in ['q', 'Q']: break
-        except:
-            pass
+    pass
 
 if __name__ == "__main__":
     main()
