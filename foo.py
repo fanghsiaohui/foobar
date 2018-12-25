7cfdc237d62cd20d4bcb86651d151fcad56d5301
fang <fang@arch>
2018-12-25 12:26:17 +0800

    init py

--- foo.py
+++ foo.py
@@ -3,37 +3,8 @@
 # from functools import wraps
 # pdb.set_trace()
 
-class Person:
-    cnt = 0
-    def __init__(self, name, gender="male", age=20):
-        self.name = name
-        self.gender = gender
-        self.age = age
-        Person.cnt += 1
-    def show(self, end='\n'):
-        print("name: {:>6},\tgender:\t{:>6},\tage: {:>2}".format(
-            self.name, self.gender, self.age), end=end)
+def main():
+    pass
 
-class Employee(Person):
-    cnt = 0
-    def __init__(self, name, gender="male", age=20,
-            zw="worker", salary=3000):
-        Person.__init__(self, name=name, gender=gender, age=age)
-        self.zw = zw
-        self.salary = salary
-        Employee.cnt += 1
-    def show(self):
-        Person.show(self, end=',\t')
-        print("zw: {:>0},\tsalary: {:>6d}".format(
-            self.zw, self.salary))
-
-zhao = Person(name="zhao")
-qian = Person(name="qian", gender="female", age=18)
-sun = Employee(name="sun")
-li = Employee(name="li", zw="artist", salary=5000)
-print("number of person: {:2d}".format(Person.cnt))
-print("number of Employee: {:2d}".format(Employee.cnt))
-zhao.show()
-qian.show()
-sun.show()
-li.show()
+if __name__ == "__main__":
+    main()
