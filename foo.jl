978bd3d9c64da0d7e58601035a0f0678d9462972
fang <fang@arch>
2018-12-29 00:05:05 +0800

    foo

--- foo.jl
+++ foo.jl
@@ -1,12 +1,7 @@
 #!/usr/bin/env julia
-function fn(x, μ, δ)
-    y = 1 / (√(2*pi) * δ) * ℯ^(-(x - μ)^2 / (2 * δ^2))
-end
-
-function f(m)
-    m = m
-    function g(n)
-        return m*n
-    end
-    return g
+using UnicodePlots
+for i in 1:5
+    a = randn(100000)
+    plot = histogram(a)
+    display(plot)
 end
--- foo.py
+++ foo.py
@@ -12,3 +12,8 @@ s = """改革春风吹满地
 中国人民真争气
 这个世界太疯狂
 耗子给猫当伴娘"""
+
+a=np.random.randn(100000)
+plt.figure()
+plt.bar(a)
+plt.show()
