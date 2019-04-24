3f82aed8fb3d2419947dbd504f6b3fd085d3ea09
fang <fang@arch>
2019-04-25 00:04:57 +0800

    foo

--- bar.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.optimize import fsolve
-from pprint import pprint
-
--- foo.py
+++ foo.py
@@ -5,16 +5,20 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from pprint import pprint
+from scipy.optimize import fsolve
 
 def f1(x, mu=0, delta=1):
     return 1/(np.sqrt(2*np.pi)*delta)*np.exp(-((x-mu)**2)/(2*delta**2))
 
 def f2(x):
     return x**2
-x = np.linspace(-10,10,201)
-plt.subplot(211)
-plt.plot(x, f1(x))
-plt.subplot(212)
-plt.plot(x, f2(x))
-#plt.show()
-plt.savefig("a.png")
+
+if __name__ == "__main__":
+    x = np.linspace(-10,10,201)
+    plt.subplot(211)
+    plt.plot(x, f1(x))
+    plt.subplot(212)
+    plt.plot(x, f2(x))
+    plt.savefig("a.png")
+    plt.show()
