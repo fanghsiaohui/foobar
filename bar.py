b819f2ab5f6b8d215e5295039df4cf360cedcd98
fang <fang@arch>
2019-04-24 23:14:07 +0800

    ba

--- bar.py
+++ bar.py
@@ -5,10 +5,3 @@ import matplotlib.pyplot as plt
 from scipy.optimize import fsolve
 from pprint import pprint
 
-def f(t):
-    x, y = t
-    f1 = x**2 + y**2 - 16
-    f2 = x - y - 4
-    return f1,f2
-
-solve = fsolve(f, [1, 1])
