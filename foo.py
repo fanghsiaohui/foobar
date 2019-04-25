050435d0ac79e20aaab77670116c415fba79755e
fang <fang@arch>
2019-04-26 03:24:34 +0800

    foo.py

--- foo.py
+++ foo.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
-# import pdb, sys, os, time, re, shutil, command
-# from functools import wraps
-# pdb.set_trace()
+#import pdb, sys, os, time, re, shutil, command
+#pdb.set_trace()
+#from functools import wraps
 
 import numpy as np
 from scipy.optimize import fsolve
+import time
+from functools import wraps
 
 def f1(x, mu=0, delta=1):
     return 1/(np.sqrt(2*np.pi)*delta)*np.exp(-((x-mu)**2)/(2*delta**2))
@@ -12,53 +14,69 @@ def f1(x, mu=0, delta=1):
 def f2(x):
     return x**2
 
-if __name__ == "__main__":
-    print("hello")
+def timer(func):
+    @wraps(func)
+    def wrapper(*args, **kw):
+        start = time.perf_counter()
+        func(*args, **kw)
+        end = time.perf_counter()
+        print("time using: {} ms".format(round((end - start) * 1000, 3)))
+    return wrapper
 
-    '''
-    # part 1: plot
-    import matplotlib.pyplot as plt
-    x = np.linspace(-10,10,201)
+@timer
+def plot_test():
+    x = np.linspace(-10, 10, 201)
     plt.subplot(211)
     plt.plot(x, f1(x))
     plt.subplot(212)
     plt.plot(x, f2(x))
     plt.savefig("a.png")
-    plt.show()
-    '''
 
-    '''
-    # part 2
-    from sympy import symbols, solve, latex, pprint
-    a, b, c = symbols('a, b, c')
-    x, y = symbols('x, y')
-    '''
+@timer
+def solution_test():
+    a, b, c, x = symbols("a, b, c, x")
+    y = a * x**2 + b * x + c
+    result = solve(y, x)
+    pprint(result)
 
-    '''
-    # part 3: millon data comoute speed
-    import random, time
-
+@timer
+def computer_test():
     random.seed(10)
-    t0 = time.perf_counter()
-    x1 = [random.randrange(1,101) for i in range(1000000)]
-    t1 = time.perf_counter()
-    x2 = [i**2 for i in x1]
-    t2 = time.perf_counter()
-    print("random:")
-    print("last 5 item of x1:\n\t{}".format(x1[-5:]))
-    print("gen 1 millon random data, time:\n\t{}".format(t1-t0))
-    print("last 5 item of x2:\n\t{}".format(x2[-5:]))
-    print("compute 1 millon data, time:\n\t{}".format(t2-t1))
+    timeRandom0 = time.perf_counter()
+    xRandom0 = [random.randint(1, 100) for i in range(1000000)]
+    timeRandom1 = time.perf_counter()
+    xRandom1 = [i**2 for i in xRandom0]
+    timeRandom2 = time.perf_counter()
+    print("\nrandom:")
+    print("last 5 data before:\n\t{}".format(xRandom0[-5:]))
+    print("last 5 data after:\n\t{}".format(xRandom1[-5:]))
+    print("generate 1 millon random data, time:\n\t{} s".format(round(timeRandom1 - timeRandom0, 6)))
+    print("compute 1 millon data, time:\n\t{} s".format(round(timeRandom2 - timeRandom1, 6)))
 
     np.random.seed(10)
-    m0 = time.perf_counter()
-    n1 = np.random.randint(1,101,1000000)
-    m1 = time.perf_counter()
-    n2 = n1**2
-    m2 = time.perf_counter()
+    timeNumpy0 = time.perf_counter()
+    xNumpy0 = np.random.randint(1, 101, 1000000)
+    timeNumpy1 = time.perf_counter()
+    xNumpy1 = xNumpy0 ** 2
+    timeNumpy2 = time.perf_counter()
     print("\nnumpy.random:")
-    print("last 5 item of n1:\n\t{}".format(n1[-5:]))
-    print("gen 1 millon random data, time:\n\t{}".format(m1-m0))
-    print("last 5 item of n2:\n\t{}".format(n2[-5:]))
-    print("compute 1 millon data, time:\n\t{}".format(m2-m1))
-    '''
+    print("last 5 data before:\n\t{}".format(xNumpy0[-5:]))
+    print("last 5 data after:\n\t{}".format(xNumpy1[-5:]))
+    print("generate 1 millon random data, time:\n\t{} s".format(round(timeNumpy1 - timeNumpy0, 6)))
+    print("compute 1 millon data, time:\n\t{} s".format(round(timeNumpy2 - timeNumpy1, 6)))
+
+if __name__ == "__main__":
+    print("hello")
+
+    #part 1: plot
+    #import matplotlib.pyplot as plt
+    #plot_test()
+    #plt.show()
+
+    #part 2
+    #from sympy import symbols, solve, latex, pprint
+    #solution_test()
+
+    #part 3: millon data comoute speed
+    #import random
+    #computer_test()
