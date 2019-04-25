056fccc5552507641d088c9b8e2ac3e4adeca0fb
fang <fang@termux>
2019-04-25 16:52:29 +0800

    foo math

--- foo.py
+++ foo.py
@@ -5,8 +5,8 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from pprint import pprint
 from scipy.optimize import fsolve
+from sympy import symbols, solve, latex, pprint,E,I
 
 def f1(x, mu=0, delta=1):
     return 1/(np.sqrt(2*np.pi)*delta)*np.exp(-((x-mu)**2)/(2*delta**2))
@@ -15,6 +15,10 @@ def f2(x):
     return x**2
 
 if __name__ == "__main__":
+    print("hello")
+    a, b, c = symbols('a, b, c')
+    x, y = symbols('x, y')
+    '''
     x = np.linspace(-10,10,201)
     plt.subplot(211)
     plt.plot(x, f1(x))
@@ -22,3 +26,4 @@ if __name__ == "__main__":
     plt.plot(x, f2(x))
     plt.savefig("a.png")
     plt.show()
+    '''
