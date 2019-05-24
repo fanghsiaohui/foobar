228ecebaae545c87a6ce528cfd849fbbe66eca90
fang <fang@termux>
2019-05-24 17:04:06 +0800

    go

--- doo.go
+++ doo.go
@@ -1,7 +1,9 @@
 package main
-//import "fmt"
+import "fmt"
 
 func main(){
-    a := []float64{}
-    println(a[:])
+    a := []float64{1,2,3}
+    b := 9
+    fmt.Println(a[:], b)
+    println(a, b)
 }
