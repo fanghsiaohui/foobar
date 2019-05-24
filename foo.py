dcd212967a8e415f0f6a4b7f85bf74b9c28863dc
fang <fang@termux>
2019-05-25 01:22:12 +0800

    foo

--- foo.py
+++ foo.py
@@ -1,10 +1,4 @@
 #!/data/data/com.termux/files/usr/bin/env python
 def f():
-    a, b = 0, 1
-    c, d = "hello", "world"
-    return a, b, c, d
+    pass
 
-*t, a, b = f()
-print(t)
-print(a, b)
-print(hex(id(a)), hex(id(b)))
