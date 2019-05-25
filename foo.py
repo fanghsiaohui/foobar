a68cc0b3ba10a4a04abcd8fbbbda963ee3a553f5
fang <fang@termux>
2019-05-25 19:09:32 +0800

    go

--- foo.py
+++ foo.py
@@ -1,4 +1,12 @@
 #!/data/data/com.termux/files/usr/bin/env python
-def f():
-    pass
+import numpy as np
+import matplotlib.pyplot as plt
 
+a = np.linspace(0, 10, 101)
+b = np.cos(a)
+c = np.sin(a)
+
+plt.grid(True)
+plt.plot(a, b, a, c)
+plt.legend(["cosine", "sine"])
+plt.savefig("tri", dpi=1200)
