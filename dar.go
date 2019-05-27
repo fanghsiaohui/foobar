063fc47d3ccc48aa5d44cc1003f1c7d716a3767e
fang <fang@termux>
2019-05-27 15:24:25 +0800

    ignore

--- dar.go
+++ dar.go
@@ -5,6 +5,14 @@ import (
     //"net/http"
 )
 func main(){
-    fmt.Println("hello, world")
-    fmt.Println(os.Args)
+    fmt.Println(len(os.Args), os.Args)
+    a, b := 0, 1
+    var p *int
+    for i := 0; i < 10; i++ {
+        a, b = b, a + b
+        p = &i
+        fmt.Println(i, a)
+        fmt.Println(*p)
+    }
+    fmt.Println(*p, a)
 }
