b4d39d81c701cb429d0489521b1d1a8a03aab048
fang <fang@termux>
2019-06-08 18:41:29 +0800

    py

--- foo.py
+++ foo.py
@@ -1,11 +1,2 @@
 #!/usr/bin/env python
-import random, pdb, time
-import numpy as np
 
-with open("weight") as f:
-    text = f.readlines()
-text.pop(0)
-start = time.time()
-text = [[eval(j) for j in i.split()] for i in text]
-elapse = time.time() - start
-print(elapse, "s")
