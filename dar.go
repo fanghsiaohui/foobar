5228b8eb1050222b6bccd33c7d1b0e04057e3b23
fang <fang@termux>
2019-06-14 17:44:09 +0800

    pi

--- dar.go
+++ dar.go
@@ -1,7 +1,28 @@
 package main
 import (
     "fmt"
+    "math/rand"
+    "time"
 )
 
+func pi(n int) float64{
+    m := 0
+    for i := 0; i < n; i++ {
+        x := rand.Float64()
+        y := rand.Float64()
+        if x * x + y * y < 1 {
+            m++
+        }
+    }
+    return float64(m)/float64(n)*4
+}
 func main(){
+    n := 100
+    for i:=0;i<5;i++ {
+        n *= 10
+        start := time.Now()
+        p := pi(n)
+        t := time.Since(start).Seconds()
+        fmt.Println(n, p, t)
+    }
 }
