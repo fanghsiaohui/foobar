dd0f8eb11396a44c9bc7234158354600323ba877
fang <fang@arch>
2019-10-02 12:28:40 +0800

    foo

--- foo.py
+++ foo.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python
 
 import time, random
+import os, sys
 import pdb
-def pi(n):
-    m = 0
-    for i in range(n):
-        x = random.random()
-        y = random.random()
-        if x**2 + y**2 < 1:
-            m += 1
-    return m/n*4
 
-n = 100
-for i in range(3):
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f():
+    pass
+
+if __name__ == "__main__":
     #pdb.set_trace()
-    n *= 10
-    start = time.time()
-    p = pi(n)
-    end = time.time()
-    print("%.2e, %.8f, %.6f" % (n, p, end-start))
+    f()
