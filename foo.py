e83cb7e470295c23f532cf25669041a4c93a08e1
fang <fang@arch>
2019-10-02 13:36:00 +0800

    plot func

--- foo.py
+++ foo.py
@@ -7,9 +7,36 @@ import pdb
 import numpy as np
 import matplotlib.pyplot as plt
 
-def f():
+from numpy import pi, sqrt, exp
+from sympy import symbols, solve, latex, pprint
+from functools import wraps
+
+#import pandas as pd
+#from scipy.optimize import fsolve
+
+def f0():
     pass
+
+def plot(func):
+    @wraps(func)
+    def wrapper(*args, **kw):
+        plt.plot(x, func(x))
+        plt.savefig("func.png")
+        plt.show()
+    return wrapper
+
+@plot
+def f1(x, mu=0, delta=1):
+    return 1 / (sqrt(2 * pi) * delta) * exp(-(x - mu) ** 2) / (2 * delta ** 2)
+
+def f2():
+    a, b, c, x = symbols("a, b, c, x")
+    y = a * x**2 + b * x +c
+    result = solve(y, x)
+    pprint(result)
 
 if __name__ == "__main__":
     #pdb.set_trace()
-    f()
+    x = np.linspace(-5,5,201)
+    f1(x)
+    f2()
