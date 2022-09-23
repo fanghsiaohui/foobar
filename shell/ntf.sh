55252e154a457bfdeb86e57d5ee42ca889592a53
fang <fang@phone>
2022-09-23 15:13:28 +0800

    rm

--- shell/ntf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-while true;do
-    a=$(date +%R)
-    if [ $a == '06:20' ]; then
-        echo -e "\nALERT:\n\ttime now\n\tgo out"
-        break
-    fi
-    sleep 60
-done
--- shell/rename_string.sh
+++
@@ -1,8 +0,0 @@
-#/usr/bin/sh
-d=${HOME}/bar
-for i in zp/* ;do
-    #f=$(basename $i)
-    f="${i#*/}"
-    echo $f
-    touch "$d/$f"
-done
